ed5e502d71eb3adfc30968fe69afcdaddc7f4814
jiaowagn <wangjiaohere@gmail.com>
2018-05-02 20:59:00 +0800

    unlike/like with Ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,13 +19,12 @@ class PostsController < ApplicationController
     unless @post.find_like(current_user)
       Like.create(:user => current_user, :post => @post)
     end
-    redirect_to posts_path
   end
 
   def unlike
     like = @post.find_like(current_user)
     like.destroy
-    redirect_to posts_path
+    render "like"
   end
 
   protected
