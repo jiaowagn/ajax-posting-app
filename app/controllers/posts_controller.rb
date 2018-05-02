bbed352ac54c9e52dc5e28d90f2a18e6e8964628
jiaowagn <wangjiaohere@gmail.com>
2018-05-02 20:48:19 +0800

    people can like/unlike post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:create, :destroy]
+  before_action :authenticate_user!, :only => [:create, :destroy, :like, :unlike]
+  before_action :find_post, :only => [:destroy, :like, :unlike]
   def index
     @posts = Post.order("id DESC").all
   end
@@ -11,11 +12,27 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = current_user.posts.find(params[:id])
     @post.destroy
   end
 
+  def like
+    unless @post.find_like(current_user)
+      Like.create(:user => current_user, :post => @post)
+    end
+    redirect_to posts_path
+  end
+
+  def unlike
+    like = @post.find_like(current_user)
+    like.destroy
+    redirect_to posts_path
+  end
+
   protected
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
     def post_params
       params.require(:post).permit(:content)
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy 
+  has_many :liked_users, :through => :likes, :source => :user
+
   validates_presence_of :content
+
+  def find_like(user)
+    self.likes.where(:user_id => user.id).first
+  end
 end
